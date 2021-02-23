cd404e61e280ad9b4b7620419fab220da2fe372a
rcparent17 <reilly.c.parent@gmail.com>
2021-02-23 18:11:26 -0500

    resolved some bugs in database add methods

--- src/server/databaseHelper.py
+++ src/server/databaseHelper.py
@@ -1,8 +1,9 @@
 import sqlite3
 
+dbPath = "../../database/motorcycle_shop.db"
 
 def addEmployeeWorkTime(full_name, hours, last_clock_in, last_clock_out):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'INSERT INTO "Employee Work Time"(FullName,Hours,"Last Clock In","Last Clock Out") VALUES(?,?,?,?)'
     args = (full_name, hours, last_clock_in, last_clock_out)
     cursor = conn.cursor()
@@ -11,10 +12,10 @@ def addEmployeeWorkTime(full_name, hours, last_clock_in, last_clock_out):
     conn.close()
 
 
-def addTransaction(name, cost, stock):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
-    query = "INSERT INTO Transaction(Name,Cost,Stock) VALUES(?,?,?)"
-    args = (name, cost, stock)
+def addTransaction(cost, time, items, name):
+    conn = sqlite3.connect(dbPath)
+    query = 'INSERT INTO Transactions ("Total Cost",Time,Items,"Customer Name") VALUES(?,?,?,?)'
+    args = (cost, time, items, name)
     cursor = conn.cursor()
     cursor.execute(query, args)
     conn.commit()
@@ -22,7 +23,7 @@ def addTransaction(name, cost, stock):
 
 
 def addUserAccesses(username, time, type):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'INSERT INTO "User Accesses"(Username,Time,Type) VALUES(?,?,?)'
     args = (username, time, type)
     cursor = conn.cursor()
@@ -32,8 +33,8 @@ def addUserAccesses(username, time, type):
 
 
 def addUser(username, permissions, password):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
-    query = "INSERT INTO Users(Username,Time,Type) VALUES(?,?,?)"
+    conn = sqlite3.connect(dbPath)
+    query = "INSERT INTO Users(Username,Permissions,Password) VALUES(?,?,?)"
     args = (username, permissions, password)
     cursor = conn.cursor()
     cursor.execute(query, args)
@@ -42,7 +43,7 @@ def addUser(username, permissions, password):
 
 
 def addWorkOrder(name, items, time_created, time_started, time_finished):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'INSERT INTO "Work Orders"(Name,Items,"Time Created","Time Started","Time Finished") VALUES(?,?,?,?,?)'
     args = (name, items, time_created, time_started, time_finished)
     cursor = conn.cursor()
@@ -52,7 +53,7 @@ def addWorkOrder(name, items, time_created, time_started, time_finished):
 
 
 def getEmployeeWorkTimeByFullName(full_name):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'SELECT * FROM "Employee Work Time" WHERE FullName=?'
     cursor = conn.cursor()
     args = (full_name,)
@@ -63,7 +64,7 @@ def getEmployeeWorkTimeByFullName(full_name):
 
 
 def getInventoryByID(ID):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = "SELECT * FROM Inventory WHERE ID=?"
     cursor = conn.cursor()
     args = (ID,)
@@ -74,7 +75,7 @@ def getInventoryByID(ID):
 
 
 def getTransactionByID(ID):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = "SELECT * FROM Transaction WHERE ID=?"
     cursor = conn.cursor()
     args = (ID,)
@@ -85,7 +86,7 @@ def getTransactionByID(ID):
 
 
 def getUserAccessesByID(ID):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'SELECT * FROM "User Accesses" WHERE ID=?'
     cursor = conn.cursor()
     args = (ID,)
@@ -96,7 +97,7 @@ def getUserAccessesByID(ID):
 
 
 def getUsersByUsername(username):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = "SELECT * FROM Users WHERE Username=?"
     cursor = conn.cursor()
     args = (username,)
@@ -107,7 +108,7 @@ def getUsersByUsername(username):
 
 
 def getWorkOrdersByID(ID):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'SELECT * FROM "Work Orders" WHERE ID=?'
     cursor = conn.cursor()
     args = (ID,)
@@ -118,7 +119,7 @@ def getWorkOrdersByID(ID):
 
 
 def updateEmployeeWorkTime(full_name, hours, last_clock_in, last_clock_out):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'UPDATE "Employee Work Time" SET Hours=?, "Last Clock In"=?, "Last Clock Out"=?  WHERE FullName=?'
     cursor = conn.cursor()
     args = (hours, last_clock_in, last_clock_out, full_name)
@@ -128,7 +129,7 @@ def updateEmployeeWorkTime(full_name, hours, last_clock_in, last_clock_out):
 
 
 def updateInventory(ID, name, cost, stock):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = "UPDATE Inventory SET Name=?, Cost=?, Stock=?  WHERE ID=?"
     cursor = conn.cursor()
     args = (name, cost, stock, ID)
@@ -136,8 +137,9 @@ def updateInventory(ID, name, cost, stock):
     conn.commit()
     conn.close()
 
+
 def addInventory(name, cost, stock):
-    conn = sqlite3.connect("../../database/motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'INSERT INTO Inventory (Name,Cost,Stock) VALUES(?,?,?)'
     args = (name, cost, stock)
     cursor = conn.cursor()
@@ -145,8 +147,9 @@ def addInventory(name, cost, stock):
     conn.commit()
     conn.close()
 
+
 def updateTransactions(ID, total_cost, time, items, customer_name):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'UPDATE Transaction SET "Total Cost"=?, Time=?, Items=?, "Customer Name"=?  WHERE ID=?'
     cursor = conn.cursor()
     args = (total_cost, time, items, customer_name, ID)
@@ -156,7 +159,7 @@ def updateTransactions(ID, total_cost, time, items, customer_name):
 
 
 def updateUserAccesses(ID, username, time, type):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'UPDATE "User Accesses" SET Username=?, Time=?, Time=?  WHERE ID=?'
     cursor = conn.cursor()
     args = (username, time, type, ID)
@@ -166,7 +169,7 @@ def updateUserAccesses(ID, username, time, type):
 
 
 def updateUser(username, permission, password):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = "UPDATE User SET Permissions=?, Password=?  WHERE Username=?"
     cursor = conn.cursor()
     args = (permission, password, username)
@@ -176,7 +179,7 @@ def updateUser(username, permission, password):
 
 
 def updateWorkOrders(ID, name, items, time_created, time_started, time_finished):
-    conn = sqlite3.connect("..\\..\\database\\motorcycle_shop.db")
+    conn = sqlite3.connect(dbPath)
     query = 'UPDATE "Work Orders" SET Name=?, Items=?, "Time Created"=?, "Time Started"=?, "Time Finished"=?  WHERE ID=?'
     cursor = conn.cursor()
     args = (name, items, time_created, time_started, time_finished, ID)
